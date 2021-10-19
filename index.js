c64817b61951d92dcf29694485c6807087076b06
zivserphos <zivfromisrael@gmail.com>
2021-10-19 19:46:26 +0300

    maze-challenge using promises

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 const { error } = require("console");
 const fs = require("fs");
+const { readFile } = require("fs/promises");
 const path = require("path");
 const generateMaze = require("./maze_generator/mazeGenerator");
+generateMaze("maze-1" , 5, 5, 22)
 
 async function promiseTreasure(roomPath) {
-   return new promise((resolve ,reject) => {
-     fs.access(path.resolve(__dirname , roomPath))
-     .then(fs.readdir)
-     .catch((err) => console.log(error))
-    
+  fs.promises.access(path.resolve(__dirname , roomPath))
+  .then(() => fs.promises.readdir(path.resolve(__dirname , roomPath))
+  .then((files) => {
+    fs.promises.appendFile("map.txt", `.${roomPath.split("maze-1")[1]}\n`)
+    for(let file of files) {
+      fs.promises.stat(path.resolve(roomPath, file))
+      .then((stats) => {
+        if (stats.isFile()) {
+          promiseChest(path.resolve(roomPath , file))
+          .then(() => console.log("roompath"))
+        }
+      }).catch(console.error("NO STATS"))
+
+    }
+  }).catch("No directory")
+  ).catch(console.error("File not exist"))
+}
+
+
+async function promiseChest(roomPath) {
+  fs.promises.readFile(path.resolve(__dirname , roomPath))
+  .then((data) => {
+    try{
+      const {treasure , clue} = JSON.parse(data)
+      if(clue) {
+        console.log(clue)
+        const roomPath = clue;
+        promiseTreasure(roomPath)
+      }
+      if (treasure) {
+        console.log(treasure)
+      }
+    }
+    catch {
+      console.log("not a valid json")
+    }
   })
+  .catch("CANT READ FILE")
 }
-async function promiseTreasure(roomPath) {
 
-}
-async function drawMapSync(currentRoomPath) {
 
-}
+promiseTreasure("maze-1")
+.then((treasure) => console.log(`Found treasure: ${treasure}`))
+
 
