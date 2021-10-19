9f81ef05ab929da839a0c098ac479f0dc8d8c048
zivserphos <zivfromisrael@gmail.com>
2021-10-19 11:58:10 +0300

    updated

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
 const fs = require("fs");
-const { dirname } = require("path");
-const path = require('path');
+const path = require("path");
 
 function findTreasureSync(roomPath) {
-    fs.access(path.relative(__dirname , roomPath), fs.R_OK , (err) => { // checks if it is a valdi path
-        if (!err) { // valid
-            if (!roomPath.includes(".json")) { 
-                const fileContent = fs.readdirSync(path.relative(__dirname , roomPath)) // read dir
-                fileContent.forEach((path) => { // go through all the files in the directory
-                    openChestSync(roomPath + "/" + path) // check chests
-            })}
-            else {
-                    openChestSync(roomPath) // read json
-                }
+  // TODO: change to fs.accessSync
+  try {
+    fs.accessSync(path.resolve(__dirname, roomPath) , fs.R_OK)
+      const dirContent = fs.readdirSync(path.relative(__dirname, roomPath)); // read dir
+        fs.appendFileSync("map.txt", (`.${roomPath.split("mazeX")[1]}\n`));
+        dirContent.forEach((dirName) => {
+        const stats = fs.statSync(path.resolve(roomPath ,dirName))
+        if (!stats.isDirectory()) {
+          console.log(openChestSync(path.resolve(roomPath , dirName))) // check chests
         }
-        else {
-            console.log("error")
-        }
-    })
-     
-}
+      })
+    }
+  catch {
+    //console.log(roomPath)
+  }}
 
 function openChestSync(chestPath) {
-    try {
-        const fileContent = JSON.parse(fs.readFileSync(path.relative(__dirname , chestPath))) // valid json
-        if (fileContent.clue) {
-            findTreasureSync(fileContent.clue)
-        }
-        if (fileContent.treasure) {
-            console.log("MONEY");
-        }
+  try {
+    const fileContent = JSON.parse(
+      fs.readFileSync(path.relative(__dirname, chestPath))
+    ); // valid json
+    if (fileContent.clue) {
+      findTreasureSync(fileContent.clue);
     }
-    catch {
-        if(!chestPath.includes(".json")){
-            findTreasureSync(chestPath)
-        }
+    //console.log(fileContent)
+    if (fileContent.treasure) {
+      fs.appendFileSync("map.txt", "money");
     }
+  } catch {
+    //console.log(chestPath)
+  }
 }
+findTreasureSync("mazeX");
 
-findTreasureSync("mazeX")
+
+// function findTreasureSync(roomPath) {
+//   // TODO: change to fs.accessSync
+//   fs.accesss(path.relative(__dirname, roomPath), fs.R_OK, (err) => {
+//     // checks if it is a valdi path
+//     if (err) {
+//       console.log("error");
+//       return;
+//     } 
+//     // valid
+//     const dirContent = fs.readdirSync(path.relative(__dirname, roomPath)); // read dir
+//     dirContent.forEach((dirName) => {
+//       // go through all the files in the directory
+//       // Check if path is directory or file
+//       // TODO: BUG: cant tell directory from file if both include .json
+//       if (dirName.includes(".json")) {
+//         openChestSync(roomPath); // read json
+//       }
+//     });
+//   });
+// }
