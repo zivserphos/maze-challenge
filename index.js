702ee5a192b74e0204cf441114c19267cc3724b6
zivserphos <zivfromisrael@gmail.com>
2021-10-19 13:28:49 +0300

    starting to built our a sync function

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const fs = require("fs");
 const path = require("path");
 const generateMaze = require("./maze_generator/mazeGenerator");
-generateMaze("maze-1", 5, 5, 22);
+//generateMaze("maze-1", 5, 5, 22);
+
+
+function findTreasureASync(roomPath , callback){
+  try {
+    fs.access(path.resolve(__dirname, roomPath) , fs.R_OK , (err) => {
+      if (err)
+        console.error('No Read access');
+      else {
+          fs.readdir(path.relative(__dirname, roomPath) , (err,files) => {
+          if (err) {
+            console.log(err)
+          }
+          else {
+            fs.appendFile("map.txt", (`.${roomPath.split("mazeX")[1]}\n`) , (err) => {
+              if(!err) {
+                  files.forEach((dirName) => {
+                  const stats = fs.statSync(path.resolve(roomPath ,dirName))
+                  if (!stats.isDirectory()) {
+                  openChestSync(path.resolve(roomPath , dirName)) // check chests
+              }
+            });
+          }
+        })
+
+          }
+        }); // read dir
+    }
+      
+    })
+  }
+  catch {
+
+  }
+}
+
 
 function findTreasureSync(roomPath) {
   // TODO: change to fs.accessSync
   try {
     fs.accessSync(path.resolve(__dirname, roomPath) , fs.R_OK)
       const dirContent = fs.readdirSync(path.relative(__dirname, roomPath)); // read dir
-        fs.appendFileSync("map.txt", (`.${roomPath.split("maze-1")[1]}\n`));
+        fs.appendFileSync("map.txt", (`.${roomPath.split("mazeX")[1]}\n`));
         dirContent.forEach((dirName) => {
         const stats = fs.statSync(path.resolve(roomPath ,dirName))
         if (!stats.isDirectory()) {
@@ -25,7 +60,7 @@ function openChestSync(chestPath) {
       fs.readFileSync(path.relative(__dirname, chestPath))
     ); // valid json
     if (fileContent.clue) {
-      findTreasureSync(fileContent.clue);
+      findTreasureASync(fileContent.clue);
     }
     //console.log(fileContent)
     if (fileContent.treasure) {
@@ -35,4 +70,5 @@ function openChestSync(chestPath) {
     //console.log(chestPath)
   }
 }
-findTreasureSync("maze-1");
+
+findTreasureASync("mazeX")
